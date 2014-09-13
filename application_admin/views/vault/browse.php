cdf75ea55f418bd4b5245db25b7f65da45adc0d7
Mark Cunningham <cmarkme@gmail.com>
2014-09-13 16:22:27 +0100

    prs1 on views/vault/browse.php

--- application_admin/views/vault/browse.php
+++ application_admin/views/vault/browse.php
@@ -1,21 +1,25 @@
-<h1><?=$virtual_root?></h1>
-<h2><?='/'.$path_in_url?></h2>
+<h1><?= $virtual_root ?></h1>
+<h2><?= '/' . $path_in_url ?></h2>
 
 <?php
-$prefix = $controller.'/'.$virtual_root.'/'.$path_in_url;
-if (!empty($dirs)) foreach( $dirs as $dir )
-    echo 'dir <a href="'.$file['name'].'">'.  $dir['name'].'</a><br>';
+$prefix = $controller . '/' . $virtual_root . '/' . $path_in_url;
+if (!empty($dirs)) {
+    foreach ($dirs as $dir)
+    {
+        echo 'dir <a href="' . $file['name'] . '">' . $dir['name'] . '</a><br>';
+    }
+}
 
-if (!empty($files)) foreach( $files as $file )
-	{
-	foreach($hashFiles as $k => $v)
-		{
-		if($v['hash']==$file['name'])
-		{
+if (!empty($files)) {
+    foreach ($files as $file)
+    {
+        foreach ($hashFiles as $k => $v)
+        {
+            if ($v['hash'] == $file['name']) {
 
-    		echo '<a href="'.$file['name'].'">'. $v['file'].'</a><br>';			
-		}
-		
-		}
-	}
+                echo '<a href="' . $file['name'] . '">' . $v['file'] . '</a><br>';
+            }
+        }
+    }
+}
 ?>
